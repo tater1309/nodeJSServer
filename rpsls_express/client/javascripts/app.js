19db1de23a9ee5edd8a868428572eb6399fc69c7
Timothy Ater <tater1309@Timothys-Mac-mini.local>
2015-03-15 10:08:45 -0700

    optimized code, added css

--- rpsls_express/client/javascripts/app.js
+++ rpsls_express/client/javascripts/app.js
@@ -1,88 +1,42 @@
+"use strict";
+
+var main = function() {
+	$("#btnRock").click(function() {
+		processResults("/play/rock");
+	});
+
+	$("#btnScissors").click(function() {
+		processResults("/play/scissors");
+	});
+
+	$("#btnPaper").click(function() {
+		processResults("/play/paper");
+	});
+
+	$("#btnLizard").click(function() {
+		processResults("/play/lizard");
+	});
+
+	$("#btnSpock").click(function() {
+		processResults("/play/spock");
+	});
+}
+
 function buildResults(response) {
-	var $outcome, $wins, $losses, $ties, $resultString;
 
-	$outcome = response.outcome;
-	$wins = response.wins;
-	$losses = response.losses;
-	$ties = response.ties;
-
-	$resultString= $("<p>").text("Game Result: " + $outcome);
-	$resultString.append($("<p>").text("Total wins: " + $wins));
-	$resultString.append($("<p>").text("Total losses: " + $losses));
-	$resultString.append($("<p>").text("Total ties: " + $ties));
+	var $resultString= $("<p>").html("Game Result: " + response.outcome + "<br><br>Total Wins: " + response.wins +
+		"<br>Total losses: " + response.losses + "<br>Total ties: " + response.ties);
 
 	return $resultString;
 }
 
 function processResults(stringPath) {
-	$("main .gameResults").empty();
-
-	$.getJSON(stringPath, function(jsonresult) {
-		$result = buildResults(jsonresult);
-	});
-
-	$("main .gameResults").append($result);
-}
-
-var main = function() {
-	"use strict";
-
-	//variables
 	var $result;
 
-	$("#btnRock").click(function() {
-		processResults("/play/rock");
-		//$("main .gameResults").empty();
-
-		//$.getJSON("/play/rock", function(jsonresult) {
-		//$result = buildResults(jsonresult);
-		//});
-
-		//$("main .gameResults").append($result);
-	});
-
-	$("#btnScissors").click(function() {
-		processResults("/play/scissors");
-		//$("main .gameResults").empty();
-
-		//$.getJSON("/play/scissors", function(jsonresult) {
-		//$result = buildResults(jsonresult);
-		//});
-
-		//$("main .gameResults").append($result);
-	});
-
-	$("#btnPaper").click(function() {
-		processResults("/play/paper");
-		//$("main .gameResults").empty();
-
-		//$.getJSON("/play/paper", function(jsonresult) {
-		//$result = buildResults(jsonresult);
-		//});
-
-		//$("main .gameResults").append($result);
-	});
-
-	$("#btnLizard").click(function() {
-		processResults("/play/lizard");
-		//$("main .gameResults").empty();
-
-		//$.getJSON("/play/lizard", function(jsonresult) {
-		//$result = buildResults(jsonresult);
-		//});
-
-		//$("main .gameResults").append($result);
-	});
-
-	$("#btnSpock").click(function() {
-		processResults("/play/spock");
-		//$("main .gameResults").empty();
-
-		//$.getJSON("/play/spock", function(jsonresult) {
-		//$result = buildResults(jsonresult);
-		//});
-
-		//$("main .gameResults").append($result);
+	$.getJSON(stringPath, function(response) {
+		$("main .gameResults").empty();
+		$result = buildResults(response);
+		$("main .gameResults").append($result);
 	});
 }
 	
