b12485dc989d7b8d894aceedd0730e9aee269730
Timothy Ater <tater1309@Timothys-Mac-mini.local>
2015-03-15 21:55:34 -0700

    added question viewing

--- quickquestion/client/javascripts/app.js
+++ quickquestion/client/javascripts/app.js
@@ -1,19 +1,24 @@
 "use strict";
 
 var main = function() {
+	displayCurrentQuestions();
+
 	$("#btnLogon").click(function() {
 		logon();
 	});
 
 	$("#btnNewPost").click(function() {
-		$("#abc").show();
+		$("#popupform").show();
 		$("#btnNewPost").hide();
-	})
+	});
 
 	$("#btnSubmitPost").click(function() {
-		$("#abc").hide();
-		$("#btnNewPost").show();
-	})
+		newPost();
+	});
+
+	$("#btnRefresh").click(function() {
+		displayCurrentQuestions();
+	});
 }
 
 function logon() {
@@ -25,30 +30,57 @@ function logon() {
 	userinfo = {"username":username, "password":password};
 
 	$.post("userlogin", userinfo, function(response) {
-		console.log(response.logon);
 
 		if (response.logon) {
+			$(".logon").hide();
+			
+			$(".logoff").show();
+			$("#btnNewPost").show();
+
+			$("#hiddenUN").val(username);
 			var $newwelcome = $("<p>").html("Welcome " + username + "<br>");
 
-			//$newwelcome.append($("<button>").attr('id', "btnNewPost").text("New Post"));
 			$("header .welcome").empty();
 			$("header .welcome").append($newwelcome);
 		}
-	})
+	});
 
 	$("#username").val("");
 	$("#password").val("");
-
-	//$("main .currentquestions").html("<p>Inside logon</p>");
 }
 
-function processResults(stringPath) {
-	var $result;
+function displayCurrentQuestions() {
+	var $display = $("<p>");
+	$.getJSON("/displayQuestions", function (questionResponse){
 
-	$.getJSON(stringPath, function(response) {
-		$("main .gameResults").empty();
-		$result = buildResults(response);
-		$("main .gameResults").append($result);
+		questionResponse.forEach(function (question) {
+			$display.append($("<div>"));
+			$display.append($("<h2>").html(question.title));
+			$display.append($("<h3>").html(" Posted By: " + question.username));
+			$display.append($("<p>").html("<br />" + question.question));
+			$display.append($("<button>").text("Answer"));
+			$display.append($("<br>"));
+			$display.append($("<hr>"));
+		})
+		$("main .currentquestions").empty();
+		$("main .currentquestions").append($display);
+	})
+}
+
+function newPost() {
+	var username, title, question, postinfo;
+
+	$("#popupform").hide();
+	$("#btnNewPost").show();
+	username = $("#hiddenUN").val();
+	title = $("#popupform #title").val();
+	question = $("#popupform #question").val();
+	postinfo = {"username":username, "title":title, "question":question};
+
+	$.post("/newPost", postinfo, function(response) {
+		if (response.posted) {
+			displayCurrentQuestions();
+		}
 	});
 }
 	
--- quickquestion/server.js
+++ quickquestion/server.js
@@ -6,12 +6,26 @@ var users = [
 	["test", "test"]
 ];
 
+var allPosts = [];
+var post1 = new newPost("server", "Test Question 1", "Does this fill the array?", getTimeStamp());
+var post2 = new newPost("server", "Test Question 2", "Does this fill the array?", getTimeStamp());
+
+allPosts.push(post1);
+allPosts.push(post2);
+
 //new post object constructor
-function newPost(user, title, question, timestamp) {
-	this.user = user;
+function newPost(username, title, question, timestamp) {
+	this.username = username;
 	this.title = title;
 	this.question = question;
 	this.timestamp = timestamp;
+}
+
+//get timestamp
+function getTimeStamp() {
+	Date.now = function now() {
+		return new Date().getTime();
+	}
 }
 
 app = express();
@@ -23,6 +37,7 @@ app.use(express.urlencoded());
 //app.use(express.session({secret: '1234567890QWERTY'}));
 
 app.post("/userlogin", function (req,res) {
+	
 	var valid = false;
 	var userinfo = req.body;
 		for (var i=0; i < users.length; i++) {
@@ -36,6 +51,21 @@ app.post("/userlogin", function (req,res) {
 	else {
 		res.json({"logon":false});
 	}
+});
+
+app.get("/displayQuestions", function (req, res) {
+	res.json(allPosts);
+});
+
+app.post("/newPost", function (req, res) {
+	console.log("inside new post");
+	var post, postinfo;
+
+	postinfo = req.body;
+	post = new newPost(postinfo.username, postinfo.title, postinfo.question, getTimeStamp());
+	allPosts.push(post);
+
+	res.json({"posted":true});
 });
 
 console.log("Server is listening at http://localhost:3000/");
